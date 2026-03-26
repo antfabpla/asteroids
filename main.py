34f3680fbfcaf5b4070721de9a260c84e62d40f1
antfabpla <antfabpla@gmail.com>
2026-03-26 18:13:00 -0400

    player can now shoot bullets

--- main.py
+++ main.py
@@ -5,19 +5,26 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
     clock = pygame.time.Clock()
     dt = 0
+
     ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     field = AsteroidField()
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -29,7 +36,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        updatable.update(dt)
+        for obj in updatable:
+            obj.update(dt)
         for ast in asteroids:
             collision = ast.collides_with(ship)
             if collision == True:
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED, LINE_WIDTH
+from shot import Shot
+from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED, PLAYER_SHOOT_SPEED, LINE_WIDTH
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -38,3 +39,9 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        bullet = Shot(self.position.x, self.position.y)
+        bullet.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
