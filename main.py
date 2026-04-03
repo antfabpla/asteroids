b56b63f7232a39c97a9b63244ca7e32cf3ff9bf2
antfabpla <antfabpla@gmail.com>
2026-04-02 22:17:53 -0400

    ship accelerates, added score, added lives

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from shot import Shot
 
 def main():
     pygame.init()
+    pygame.font.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -21,9 +22,11 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    score = 0
 
     ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     field = AsteroidField()
+    score_count = pygame.font.Font(None, 36)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -44,17 +47,24 @@ def main():
                 shot_collision = ast.collides_with(shot)
                 if shot_collision == True:
                     log_event("asteroid_shot")
+                    score += 100
                     shot.kill()
                     ast.split()
             if ship_collision == True:
                 log_event("player_hit")
-                print("Game over!")
-                sys.exit()
+                ship.respawn()
+                if ship.lives <= 0:
+                    print("Game over!")
+                    print(f"Score: {score}")
+                    sys.exit()
+                
             else:
                 continue
 
         for sprite in drawable:
             sprite.draw(screen)
+        screen.blit(((pygame.font.Font(None, 36)).render(f"Score: {score}", False, (255,255,255), (0,0,0))), (0,0))
+        screen.blit(((pygame.font.Font(None, 36)).render(f"Lives: {ship.lives}", False, (255,255,255), (0,0,0))), (0,35))
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -1,14 +1,17 @@
 import pygame
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS, LINE_WIDTH
+from constants import PLAYER_RADIUS, PLAYER_INITIAL_SPEED, PLAYER_ACCELERATION, PLAYER_TURN_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS, LINE_WIDTH, SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.shot_cooldown = 0
-    
-    rotation = 0
+        self.respawn_cooldown = 0
+        self.rotation = 180
+        self.lives = 3
+        self.speed = 0
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -20,11 +23,19 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
     
+    def reset(self):
+        self.position = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        self.speed = 0
+        self.rotation = 180
+    
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
-        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        rotated_with_speed_vector = rotated_vector * self.speed * dt
         self.position += rotated_with_speed_vector
+    
+    def accelerate(self, dt, acc=PLAYER_ACCELERATION):
+        self.speed += acc * dt
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -32,11 +43,15 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
         self.shot_cooldown -= dt
-
+        if self.speed > 0:
+            self.accelerate(-dt, PLAYER_ACCELERATION/2)
+        if self.speed < 0:
+            self.accelerate(dt, PLAYER_ACCELERATION/2)
+        self.move(dt)
         if keys[pygame.K_w]:
-            self.move(dt)
+            self.accelerate(dt)
         if keys[pygame.K_s]:
-            self.move(-dt)
+            self.accelerate(-dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -51,3 +66,10 @@ class Player(CircleShape):
             self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         bullet = Shot(self.position.x, self.position.y)
         bullet.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
+    def respawn(self):
+
+        if self.lives <= 0:
+            return
+        self.lives -= 1
+        self.reset()
